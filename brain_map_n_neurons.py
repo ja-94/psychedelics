8f394aa0cde62c9be3c9a347366886cc375718e2
Guido <guido.meijer@research.fchampalimaud.org>
2023-04-17 16:34:18 +0200

    Small fixes

--- brain_map_n_neurons.py
+++ brain_map_n_neurons.py
@@ -31,23 +31,23 @@ all_neurons = all_neurons[all_neurons['region'] != 'void']
 CMAP = 'turbo'
 
 # Plot brain map slices
-f, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(6, 4), dpi=200)
+f, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(9, 4), dpi=200)
 
 plot_scalar_on_slice(all_neurons['region'].values, np.log10(all_neurons['n_neurons'].values), ax=ax1,
                      slice='coronal', coord=AP[0]*1000, background='boundary', brain_atlas=ba,
-                     mapping='Beryl', cmap=CMAP, clevels=[0, 3])
+                     mapping='Beryl', cmap=CMAP, clevels=[0, 2])
 ax1.axis('off')
 ax1.set(title=f'+{np.abs(AP[0])} mm AP')
 
 plot_scalar_on_slice(all_neurons['region'].values, np.log10(all_neurons['n_neurons'].values), ax=ax2,
                      slice='coronal', coord=AP[1]*1000, background='boundary', brain_atlas=ba,
-                     mapping='Beryl', cmap=CMAP, clevels=[0, 3])
+                     mapping='Beryl', cmap=CMAP, clevels=[0, 2])
 ax2.axis('off')
 ax2.set(title=f'-{np.abs(AP[1])} mm AP')
 
 plot_scalar_on_slice(all_neurons['region'].values, np.log10(all_neurons['n_neurons'].values), ax=ax3,
                      slice='coronal', coord=AP[2]*1000, background='boundary', brain_atlas=ba,
-                     mapping='Beryl', cmap=CMAP, clevels=[0, 3])
+                     mapping='Beryl', cmap=CMAP, clevels=[0, 2])
 ax3.axis('off')
 ax3.set(title=f'-{np.abs(AP[2])} mm AP')
 
@@ -58,9 +58,9 @@ f.subplots_adjust(right=0.85)
 # axes width 0.02 and height 0.4
 cb_ax = f.add_axes([0.88, 0.42, 0.01, 0.2])
 cbar = f.colorbar(mappable=ax1.images[0], cax=cb_ax)
-cbar.ax.set_ylabel('Total number of\nrecorded neurons (log)', rotation=270, labelpad=16)
-cbar.ax.set_yticks([0, 1, 2, 3])
-cbar.ax.set_yticklabels([1, 10, 100, 1000])
+cbar.ax.set_ylabel('Total number of\nrecorded neurons (log)', rotation=270, labelpad=25)
+cbar.ax.set_yticks([0, 1, 2])
+cbar.ax.set_yticklabels([1, 10, 100])
 
 plt.savefig(join(path_dict['fig_path'], 'brain_map_n_neurons.pdf'))
 
